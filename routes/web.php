bf9eb4c5180e64314d0886da40f47106a1e70e35
AVASH-github <nepalavash20@gmail.com>
2025-02-03 16:54:02 +0545

    Added home page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/',[HomeController::class,'homepage']);
 // Route::middleware([
